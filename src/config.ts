6aec238e5dacda038bbf2bf137b3e8d5c112d82a
Wiput Pootong <wiput.pootong@gmail.com>
2019-03-29 15:53:46 +0700

    :alien: update firebase

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,15 @@
 import * as quiz from './scenes/quiz'
 import * as vote from './scenes/vote'
-import { IConfig } from './core/model'
+import {IConfig} from './core/model'
 
 export const config: IConfig = {
   firebase: {
-    apiKey: 'AIzaSyDWaXE2bSrgg1AL4TG1rZmFC2NyUQ2_D2A',
-    authDomain: 'ingage-platform-demo.firebaseapp.com',
-    databaseURL: 'https://ingage-platform-demo.firebaseio.com',
-    projectId: 'ingage-platform-demo',
-    storageBucket: 'ingage-platform-demo.appspot.com',
-    messagingSenderId: '13020059910'
+    apiKey: 'AIzaSyD69fVmX1N539fYPjj4X2mu7hDR4LYAnL8',
+    authDomain: 'ycccamp.firebaseapp.com',
+    databaseURL: 'https://ycccamp.firebaseio.com',
+    projectId: 'ycccamp',
+    storageBucket: 'ycccamp.appspot.com',
+    messagingSenderId: '191460697180'
   },
   scenes: [quiz.scene, vote.scene]
 }
